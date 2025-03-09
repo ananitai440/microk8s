d6bbdeabd483c2a8d2871c77229442bfce3262db
ananitai440 <156970254+ananitai440@users.noreply.github.com>
2025-03-09 19:46:03 +0530

    Update microk8s_setup.sh

--- microk8s_setup.sh
+++ microk8s_setup.sh
@@ -5,5 +5,5 @@ microk8s enable dns
 microk8s enable dashboard
 microk8s enable sorage
 microk8s enable ingress
-microk8s join 192.168.1.51:25000/62203ea7df08a96f387e69c00d8de49e/49f409b1d86f --worker
+microk8s join 192.168.1.51:25000/7e069d4eb2dc3e0618f74215e2928292/b3dc388549c4 --worker
 
